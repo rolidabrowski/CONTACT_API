5afd388f787df4e53836e156ed609a5fe782a100
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-09-11 18:14:07 +0200

    added avatar parameter

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,7 +23,7 @@ export const signup = async (req, res, next) => {
     }
 
     const avatarUrl = gravatar.url(email);
-    const newUser = await User.create(body);
+    const newUser = await User.create({ ...body, avatarUrl });
     return res.status(201).json({
       status: true,
       email: newUser.email,
