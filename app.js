40b4705e79bae929e3f8a1ddb6c94e63790b8c53
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-09-17 08:29:38 +0200

    update app.js

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
 app.use(helmet());
 app.use(errorHandler);
+app.use(express.static("public"));
 
 app.use("/api/user", userRouter);
 app.use("/api/contacts", contactsRouter);
